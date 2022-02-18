b91237d98508b4cd2f4654d3e67f43543f2a3d17
BaptisS <56864846+BaptisS@users.noreply.github.com>
2022-02-18 19:36:01 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
-oci network drg-attachment delete --drg-attachment-id $sp2drgattachocid --force
-oci network drg-attachment delete --drg-attachment-id $sp1drgattachocid --force
-oci network drg-attachment delete --drg-attachment-id $drgattachocid --force
+oci network drg-attachment delete --drg-attachment-id $drg1_attach_spoke2_ocid --force
+oci network drg-attachment delete --drg-attachment-id $drg1_attach_spoke1_ocid --force
+oci network drg-attachment delete --drg-attachment-id $drg1_attach_sharedvcn_ocid --force
 
-oci network drg delete --drg-id $drgocid --force
+oci network drg delete --drg-id $drg1_ocid --force
 
-oci network subnet delete --subnet-id $sp2subnetocid --force
-oci network internet-gateway delete --ig-id $sp2igocid --force
-oci network vcn delete --vcn-id $sp2vcnocid --force
+oci compute instance terminate --instance-id $vmspoke2ocid --preserve-boot-volume false --force --wait-for-state TERMINATED --wait-interval-seconds 1
 
-oci network subnet delete --subnet-id $sp1subnetocid --force
-oci network internet-gateway delete --ig-id $sp1igocid --force
-oci network vcn delete --vcn-id $sp1vcnocid --force
+oci network route-table update --rt-id $vcn_spoke2_rt_pubsub_ocid --route-rules '[]' --force
+oci network internet-gateway delete --ig-id $vcn_spoke2_ig_ocid --force
+oci network subnet delete --subnet-id $vcn_spoke2_subnet1_ocid --force
+oci network vcn delete --vcn-id $vcn_spoke2_ocid --force
 
+oci compute instance terminate --instance-id $vmspoke1ocid --preserve-boot-volume false --force --wait-for-state TERMINATED --wait-interval-seconds 1
+
+oci network route-table update --rt-id $vcn_spoke1_rt_pubsub_ocid --route-rules '[]' --force
+oci network internet-gateway delete --ig-id $vcn_spoke1_ig_ocid --force
+oci network subnet delete --subnet-id $vcn_spoke1_subnet1_ocid --force
+oci network vcn delete --vcn-id $vcn_spoke1_ocid --force
 
 oci compute instance terminate --instance-id $vmocid --preserve-boot-volume false --force --wait-for-state TERMINATED --wait-interval-seconds 1
 
-oci network subnet delete --subnet-id $subnetocid --force
-oci network internet-gateway delete --ig-id $igocid --force
-oci network route-table delete --rt-id $sharedvcnrtdrgocid --force
-oci network vcn delete --vcn-id $vcnocid --force
+oci network route-table update --rt-id $vcn_shared_rt_pubsub_ocid --route-rules '[]' --force
+oci network subnet delete --subnet-id $vcn_shared_subnet1_ocid --force
+oci network internet-gateway delete --ig-id $vcn_shared_ig_ocid --force
+oci network route-table delete --rt-id $vcn_shared_rt_drgattach_ocid --force
+oci network vcn delete --vcn-id $vcn_shared_ocid --force
